168a004253bc7dd28e4e10920566731d210377b4
Sujith Varma EPURI <sujith.epuri@hexagon.com>
2023-06-19 11:08:47 +0530

    Updates

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
- * ATTENTION: An "eval-source-map" devtool has been used.
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
@@ -15,7 +15,7 @@
   \**********************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("Promise.all(/*! import() */[__webpack_require__.e(\"webpack_sharing_consume_default_react_react\"), __webpack_require__.e(\"src_App_js\")]).then(__webpack_require__.bind(__webpack_require__, /*! ./App */ \"./src/App.js\"));//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMuanMiLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zaGVsbC8uL3NyYy9pbmRleC5qcz8yZGVjIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgnLi9BcHAnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.js\n");
+eval("Promise.all(/*! import() */[__webpack_require__.e(\"webpack_sharing_consume_default_react_react\"), __webpack_require__.e(\"src_App_js\")]).then(__webpack_require__.bind(__webpack_require__, /*! ./App */ \"./src/App.js\"));\n\n//# sourceURL=webpack://shell/./src/index.js?");
 
 /***/ }),
 
